16ff51c73cd504a1c8fe44cc23311a81897daf73
TudorG03 <tudi.gheorghiu@gmail.com>
2023-10-15 12:58:13 +0300

    added cart context and shopping icon as well as cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,16 @@
 import { Fragment, useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import { Link, Outlet } from "react-router-dom";
 import { signUserOut } from "../../utils/firebase/firebase.utils";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -19,13 +23,17 @@ const Navigation = () => {
             SHOP
           </Link>
           {currentUser ? (
-            <span onClick={signUserOut} className="nav-link">SIGN OUT</span>
+            <span onClick={signUserOut} className="nav-link">
+              SIGN OUT
+            </span>
           ) : (
             <Link className="nav-link" to="/auth">
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,18 @@
+import { useContext } from "react";
+import { ProductsContext } from "../../contexts/product.context";
+import ProductCard from "../../components/product-card/product-card.component";
+import "./shop.styles.scss";
 
 const Shop = () => {
-    return (
-        <div>
-            SHOP
-        </div>
-    );
+  const { products } = useContext(ProductsContext);
+
+  return (
+    <div className="products-container">
+      {products.map((product) => {
+        return <ProductCard key={product.id} product={product} />;
+      })}
+    </div>
+  );
 };
 
 export default Shop;
