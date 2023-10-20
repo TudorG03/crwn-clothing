f3f6d3dcb939666f2e06d2f2731faef146a8870d
Tudor Gheorghiu <tudi.gheorghiu@gmail.com>
2023-10-20 11:58:09 +0300

    Started working on storing the products inside firebase store db

--- src/contexts/product.context.jsx
+++ src/contexts/product.context.jsx
@@ -1,12 +1,17 @@
-import { createContext, useState } from 'react';
-import PRODUCTS from '../shop-data.json';
+import { createContext, useState, useEffect } from 'react';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const ProductsContext = createContext({
     products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        getCategoriesAndDocuments();
+    });
+
     const value = { products };
 
     return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,13 +7,17 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from "firebase/auth";
 import {
   getFirestore,
   doc,
   getDoc,
-  setDoc
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs
 } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -29,20 +33,54 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({
-    prompter: "select_account"
+  prompter: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
-export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDoc(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const {title, items} = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+
+  return categoryMap;
+};
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
   if (!userAuth) return;
-  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userDocRef = doc(db, "users", userAuth.uid);
   const userSnapshot = await getDoc(userDocRef);
-  
+
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
@@ -52,12 +90,12 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
         displayName,
         email,
         createdAt,
-        ...additionalInformation
+        ...additionalInformation,
       });
-    } catch(err) {
-      console.log('error creating user ', err.message);
-    };
-  } 
+    } catch (err) {
+      console.log("error creating user ", err.message);
+    }
+  }
 
   return userDocRef;
 };
@@ -66,7 +104,7 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
 
   return await createUserWithEmailAndPassword(auth, email, password);
-}
+};
 
 export const signUserInWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
@@ -76,4 +114,5 @@ export const signUserInWithEmailAndPassword = async (email, password) => {
 
 export const signUserOut = async () => await signOut(auth);
 
-export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
